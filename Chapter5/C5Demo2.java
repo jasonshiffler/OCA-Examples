53731d03a99484f63b6e9f6c37cdad1ef8394338
Jason Shiffler <jasonshiffler@yahoo.com>
2019-06-26 07:26:26 -0500

    test

--- Chapter5/C5Demo2.java
+++ Chapter5/C5Demo2.java
@@ -5,8 +5,7 @@
  *
  *
  * -Overriding Methods
- *
- *
+ * 
  *
  * Rules of Overriding Methods
  *
@@ -19,10 +18,61 @@
  *
  * -If a method returns a value it must be the same or the subclass of the the method in the parent class. (Covariant)
  *
+ * Hidden method rules are the same with the exception that a method must be marked static in both the class and subclass
+ *
+ *
  */
 
 
 package Chapter5;
 
+import java.io.IOException;
+
 public class C5Demo2 {
+
+    class Fish {
+
+        String getNumberOfFins() throws IOException {
+            return "Undefined";
+
+        } //close method
+
+        private String getNumberOfTeeth() throws IOException {
+            return "Undefined";
+        }
+
+         final String getNumberOfBones(){
+             return "200";
+
+        }//close method
+
+    } //close class
+
+    class Shark extends Fish {
+
+
+        String getNumberOfFins(){return "3";} //Doesn't have to throw an Exception
+
+        //Must return the same type or a subclass as the overriden method
+
+        //int getNumberOfFins(){return "3";}
+
+        //Must be at least as accessible or more accessible than the method in the parent class
+
+        //private String getNumberOfFins(){return "3";}
+
+        //Can't throw a more general Exception than the overriden method
+
+        //String getNumberOfFins() throws Exception{ return "3";}
+
+
+        //Redeclaration of a private method. We aren't overloading since the parent class method is private
+        int getNumberOfTeeth() throws Exception{return 1;}
+
+
+        //Parent method is marked final so overriding is not permitted.
+        //String getNumberOfBones(){return "300";}
+    }
+
+
 }
